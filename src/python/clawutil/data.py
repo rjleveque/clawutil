e249388cf6ab735eeeb582acaee37abf863f9d6e
cvogl <cvogl>
2015-03-22 16:19:09 -0700

    Added slicedata to data.dy

--- src/python/clawutil/data.py
+++ src/python/clawutil/data.py
@@ -329,6 +329,7 @@ class ClawRunData(ClawData):
             self.add_data(amrclaw.AmrclawInputData(self.clawdata),'amrdata')
             self.add_data(amrclaw.RegionData(num_dim=num_dim),'regiondata')
             self.add_data(amrclaw.GaugeData(num_dim=num_dim),'gaugedata')
+            self.add_data(amrclaw.SliceData(num_dim=num_dim),'slicedata')
 
         elif pkg.lower() in ['geoclaw']:
 
